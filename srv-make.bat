24cc8093b0bcf61541016fc20ba6a5c7f9c08a0c
Thor Grimner <senseitg@hotmail.com>
2011-03-30 15:54:16 +0200

    On windows, srv_sdl.exe is now compiled - suitable for running as service.

--- srv-make.bat
+++ srv-make.bat
@@ -18,12 +18,13 @@ IF ERRORLEVEL 1 GOTO ERROR
 
 ECHO Linking...
 g++ oswrap.o cam.o srv.o speech.o -g -L ./lib-w32 -lkernel32 -lwsock32 -lx264 -lmsvcrt -lswscale -lavutil -lvideoinput -lddraw -ldxguid -lole32 -loleaut32 -lstrmiids -luuid -lsdl -lsam -o bin/srv.exe
+g++ oswrap.o cam.o srv.o speech.o -g -L ./lib-w32 -DUSESDL -mwindows -lmingw32 -lsdlmain -lsdl -lkernel32 -lwsock32 -lx264 -lmsvcrt -lswscale -lavutil -lvideoinput -lddraw -ldxguid -lole32 -loleaut32 -lstrmiids -luuid -lsam -o bin/srv_sdl.exe
 IF ERRORLEVEL 1 GOTO ERROR
 
 ECHO Cleaning up...
 del *.o
-REM SLEEP 1
 REM strip bin/srv.exe
+REM strip bin/srv_sdl.exe
 
 ECHO Done!
 
