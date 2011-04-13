dcfcb32837fc23d81504e8f716bcf58b3bb8d3f0
Thor Grimner <senseitg@hotmail.com>
2011-04-13 14:10:40 +0200

    Fixed crash in speech.c -STG

--- speech.c
+++ speech.c
@@ -8,9 +8,10 @@ struct sam_list_t {
 };
 typedef struct sam_list_t sam_list_t;
 
-// SAM linked-list
+// SAM linked-list and mutex
 static sam_list_t *sam_first = NULL;
 static sam_list_t *sam_last = NULL;
+static  SDL_mutex *sam_mx;
 
 // SAM buffers
 static char          sam_buffer[ 22050 * 10 ];
@@ -68,6 +69,7 @@ static void sdl_mixer( void *unused, Uint8 *stream, int len ) {
 void speech_open() {
   int n;
   float m;
+  sam_mx = SDL_CreateMutex();
   SDL_AudioSpec fmt;
   fmt.freq     = 11025;
   fmt.format   = AUDIO_U8;
@@ -100,16 +102,19 @@ void speech_poll() {
     sam_speaking = 0;
 	}
 	if( sam_speaking == 0 ) {
+    SDL_mutexP( sam_mx );
 		if( sam_first ) {
+  		// Queue pop
+      p_sam = sam_first;
+      sam_first = p_sam->next;
+      SDL_mutexV( sam_mx );
   		// Set parameters
   		sam_params( SAM_SCALE, SAM_SING, SAM_SPEED, SAM_PITCH, SAM_MOUTH, SAM_THROAT );
   		sam_size = sizeof( sam_buffer );
-  		if( sam_speak( sam_buffer, &sam_size, sam_first->phenomes ) == 0 ) sam_play();
-  		
-  		// Queue pop
-  		p_sam = sam_first->next;
-  		free( sam_first );
-  		sam_first = p_sam;
+  		if( sam_speak( sam_buffer, &sam_size, p_sam->phenomes ) == 0 ) sam_play();
+  		free( p_sam );
+		} else {
+      SDL_mutexV( sam_mx );
 		}
 	}
 }
@@ -118,6 +123,11 @@ void speech_queue( char* speak ) {
   sam_list_t *p_sam = malloc( sizeof( sam_list_t ) );
 
   if( !opened ) return;
+    
+  if( p_sam == NULL ) {
+    printf( "WHAT THE HELL? NO MEMORY?\n" );
+    return;
+  }
 
   // Copy and convert to phenomes
   memset( p_sam->phenomes, 0, 256 );
@@ -126,6 +136,7 @@ void speech_queue( char* speak ) {
   strcat( p_sam->phenomes, " \x9b\0" ); // TODO: is this really necessary?
 
  	// Queue push
+  SDL_mutexP( sam_mx );
 	p_sam->next = NULL;
 	if( sam_first ) {
 		sam_last->next = p_sam;
@@ -133,12 +144,14 @@ void speech_queue( char* speak ) {
 		sam_first = p_sam;
 	}  
 	sam_last = p_sam;
+  SDL_mutexV( sam_mx );
 	
 	speech_poll();
 
 }
 
 void speech_free() {
+  SDL_DestroyMutex( sam_mx );
 }
 
 int speech_state() {
